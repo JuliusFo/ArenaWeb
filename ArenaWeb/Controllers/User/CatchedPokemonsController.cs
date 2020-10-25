8e047e4b4dcc3e50796e0aaa1e5efee4af6202c0
Julius Forner <j.forner@ccc-software.de>
2020-10-25 23:38:47 +0100

    - Removed userId as a parameter. Using local context instead. - Reworked the display-name in several views.

--- ArenaWeb/Controllers/User/CatchedPokemonsController.cs
+++ ArenaWeb/Controllers/User/CatchedPokemonsController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ArenaWeb.Data;
+using ArenaWeb.Transfer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using ArenaWeb.Data;
-using ArenaWeb.Data.Models.Entities;
-using System.Security.Claims;
-using ArenaWeb.Transfer;
 
 namespace ArenaWeb.Controllers.User
 {
@@ -22,9 +17,10 @@ namespace ArenaWeb.Controllers.User
 		}
 
 		// GET: CatchedPokemons
-		public async Task<IActionResult> Index(string id)
+		public async Task<IActionResult> Index()
 		{
-			var catchedPokemonQuery = context.Twitchuser.Where(tu => tu.Twitchuser_Id == id);
+			var userId = User.Claims.Where(c => c.Type.Contains("nameidentifier")).FirstOrDefault()?.Value;
+			var catchedPokemonQuery = context.Twitchuser.Where(tu => tu.Twitchuser_Id == userId);
 			var catchedPokemonResult = await catchedPokemonQuery.SelectMany(u => u.CatchedPokemon.Select(c => new TransferDexElement()
 			{
 				Id = c.SdPokemon.SdPokemon_Id,
@@ -57,143 +53,6 @@ namespace ArenaWeb.Controllers.User
 			}
 
 			return View(pokedexResult);
-		}
-
-		// GET: CatchedPokemons/Details/5
-		public async Task<IActionResult> Details(decimal? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
-
-			var catchedPokemon = await context.CatchedPokemon
-				.Include(c => c.SdPokemon)
-				.Include(c => c.Twitchuser)
-				.FirstOrDefaultAsync(m => m.CatchedPokemon_Id == id);
-			if (catchedPokemon == null)
-			{
-				return NotFound();
-			}
-
-			return View(catchedPokemon);
-		}
-
-		// GET: CatchedPokemons/Create
-		public IActionResult Create()
-		{
-			ViewData["SdPokemon_Id"] = new SelectList(context.SdPokemon, "SdPokemon_Id", "SdPokemon_Id");
-			ViewData["Twitchuser_Id"] = new SelectList(context.Twitchuser, "Twitchuser_Id", "Twitchuser_Id");
-			return View();
-		}
-
-		// POST: CatchedPokemons/Create
-		// To protect from overposting attacks, enable the specific properties you want to bind to, for 
-		// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Create([Bind("CatchedPokemon_Id,Pokemon_AmountCatched,Pokemon_AmountOnFightingTeam,SdPokemon_Id,Twitchuser_Id")] CatchedPokemon catchedPokemon)
-		{
-			if (ModelState.IsValid)
-			{
-				context.Add(catchedPokemon);
-				await context.SaveChangesAsync();
-				return RedirectToAction(nameof(Index));
-			}
-			ViewData["SdPokemon_Id"] = new SelectList(context.SdPokemon, "SdPokemon_Id", "SdPokemon_Id", catchedPokemon.SdPokemon_Id);
-			ViewData["Twitchuser_Id"] = new SelectList(context.Twitchuser, "Twitchuser_Id", "Twitchuser_Id", catchedPokemon.Twitchuser_Id);
-			return View(catchedPokemon);
-		}
-
-		// GET: CatchedPokemons/Edit/5
-		public async Task<IActionResult> Edit(decimal? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
-
-			var catchedPokemon = await context.CatchedPokemon.FindAsync(id);
-			if (catchedPokemon == null)
-			{
-				return NotFound();
-			}
-			ViewData["SdPokemon_Id"] = new SelectList(context.SdPokemon, "SdPokemon_Id", "SdPokemon_Id", catchedPokemon.SdPokemon_Id);
-			ViewData["Twitchuser_Id"] = new SelectList(context.Twitchuser, "Twitchuser_Id", "Twitchuser_Id", catchedPokemon.Twitchuser_Id);
-			return View(catchedPokemon);
-		}
-
-		// POST: CatchedPokemons/Edit/5
-		// To protect from overposting attacks, enable the specific properties you want to bind to, for 
-		// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(decimal id, [Bind("CatchedPokemon_Id,Pokemon_AmountCatched,Pokemon_AmountOnFightingTeam,SdPokemon_Id,Twitchuser_Id")] CatchedPokemon catchedPokemon)
-		{
-			if (id != catchedPokemon.CatchedPokemon_Id)
-			{
-				return NotFound();
-			}
-
-			if (ModelState.IsValid)
-			{
-				try
-				{
-					context.Update(catchedPokemon);
-					await context.SaveChangesAsync();
-				}
-				catch (DbUpdateConcurrencyException)
-				{
-					if (!CatchedPokemonExists(catchedPokemon.CatchedPokemon_Id))
-					{
-						return NotFound();
-					}
-					else
-					{
-						throw;
-					}
-				}
-				return RedirectToAction(nameof(Index));
-			}
-			ViewData["SdPokemon_Id"] = new SelectList(context.SdPokemon, "SdPokemon_Id", "SdPokemon_Id", catchedPokemon.SdPokemon_Id);
-			ViewData["Twitchuser_Id"] = new SelectList(context.Twitchuser, "Twitchuser_Id", "Twitchuser_Id", catchedPokemon.Twitchuser_Id);
-			return View(catchedPokemon);
-		}
-
-		// GET: CatchedPokemons/Delete/5
-		public async Task<IActionResult> Delete(decimal? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
-
-			var catchedPokemon = await context.CatchedPokemon
-				.Include(c => c.SdPokemon)
-				.Include(c => c.Twitchuser)
-				.FirstOrDefaultAsync(m => m.CatchedPokemon_Id == id);
-			if (catchedPokemon == null)
-			{
-				return NotFound();
-			}
-
-			return View(catchedPokemon);
-		}
-
-		// POST: CatchedPokemons/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> DeleteConfirmed(decimal id)
-		{
-			var catchedPokemon = await context.CatchedPokemon.FindAsync(id);
-			context.CatchedPokemon.Remove(catchedPokemon);
-			await context.SaveChangesAsync();
-			return RedirectToAction(nameof(Index));
-		}
-
-		private bool CatchedPokemonExists(decimal id)
-		{
-			return context.CatchedPokemon.Any(e => e.CatchedPokemon_Id == id);
 		}
 	}
 }
