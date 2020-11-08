cfec26b0d584c6c4f9af44f6453dcde4d15ad728
Julius Forner <j.forner@ccc-software.de>
2020-11-08 16:54:25 +0100

    - Added user information panel.

--- ArenaWeb/Controllers/User/UserController.cs
+++ ArenaWeb/Controllers/User/UserController.cs
@@ -1,17 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ArenaWeb.Data;
+using ArenaWeb.Transfer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ArenaWeb.Controllers.User
 {
-    public class UserController : Controller
-    {
-        [HttpGet("~/account")]
-        public IActionResult SignIn() => View("Account");
+	public class UserController : Controller
+	{
+		#region Fields
 
-        [HttpGet("~/UserDex")]
-        public IActionResult UserDex() => View("UserDex");
-    }
+		private readonly AppDbContext db;
+
+		#endregion
+
+		#region Constructor
+
+		public UserController(AppDbContext db)
+		{
+			this.db = db;
+		}
+
+		#endregion
+
+		#region Properties
+
+
+
+		#endregion
+
+		#region Methods
+
+		#region Web-API
+
+		public async Task<IActionResult> Index()
+		{
+			var userId = User.Claims.Where(c => c.Type.Contains("nameidentifier")).FirstOrDefault()?.Value;
+			var query = db.Twitchuser.Where(u => u.Twitchuser_Id == userId);
+			var queryResult = await query.Select(r => new TransferUserElement()
+			{
+				UserName = r.DisplayName,
+				RegisterDt = DateTime.Now,
+				TotalPokemonCatched = r.CatchedPokemon.Sum(c => c.Pokemon_AmountCatched),
+				TotalUniquePokemonCatched = r.CatchedPokemon.Count(),
+				TotalAchievementsUnlocked = r.Achievements.Count(),
+				TotalAchievementsDone = r.Achievements.Where(a => a.LastFight != null).Count(),
+				StarterPokemon = "Platzhalter"
+			}).FirstOrDefaultAsync();
+
+			return View(queryResult);
+		}
+
+		#endregion
+
+		#endregion
+	}
 }
---
+++ ArenaWeb/Transfer/TransferUserElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArenaWeb.Transfer
+{
+	public class TransferUserElement
+	{
+		public string UserName { get; set; }
+
+		public DateTime RegisterDt { get; set; } = DateTime.Now;
+
+		public int TotalPokemonCatched { get; set; }
+
+		public int TotalUniquePokemonCatched { get; set; }
+
+		public int TotalAchievementsUnlocked { get; set; }
+
+		public int TotalAchievementsDone { get; set; }
+
+		public string StarterPokemon { get; set; }
+
+		public int TotalDaysSinceRegister
+		{
+			get
+			{
+				return (int)(DateTime.Now - RegisterDt).TotalDays;
+			}
+		}
+	}
+}
