4df37b8b6088e572a09ca928fb9b5398e1b4ed35
Julius Forner <j.forner@ccc-software.de>
2021-02-10 20:15:03 +0100

    Added base register page, added redirection after login depending on user registration state.

--- ArenaWeb/Controllers/Authentification/AuthentificationController.cs
+++ ArenaWeb/Controllers/Authentification/AuthentificationController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using ArenaWeb.Data;
 using ArenaWeb.Extensions;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -6,44 +7,66 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ArenaWeb.Controllers.Authentification
 {
-    public class AuthenticationController : Controller
-    {
-        [HttpGet("~/signin")]
-        public async Task<IActionResult> SignIn() => View("SignIn", await HttpContext.GetExternalProvidersAsync());
+	public class AuthenticationController : Controller
+	{
+		#region Fields
 
-        [HttpPost("~/signin")]
-        public async Task<IActionResult> SignIn([FromForm] string provider)
-        {
-            // Note: the "provider" parameter corresponds to the external
-            // authentication provider choosen by the user agent.
-            if (string.IsNullOrWhiteSpace(provider))
-            {
-                return BadRequest();
-            }
+		private readonly AppDbContext context;
 
-            if (!await HttpContext.IsProviderSupportedAsync(provider))
-            {
-                return BadRequest();
-            }
+		#endregion
 
-            // Instruct the middleware corresponding to the requested external identity
-            // provider to redirect the user agent to its own authorization endpoint.
-            // Note: the authenticationScheme parameter must match the value configured in Startup.cs
-            return Challenge(new AuthenticationProperties { RedirectUri = "/" }, provider);
-        }
+		#region Constructor
 
-        [HttpGet("~/signout")]
-        [HttpPost("~/signout")]
-        public IActionResult SignOut()
-        {
-            // Instruct the cookies middleware to delete the local cookie created
-            // when the user agent is redirected from the external identity provider
-            // after a successful authentication flow (e.g Google or Facebook).
-            return SignOut(new AuthenticationProperties { RedirectUri = "/" },
-                CookieAuthenticationDefaults.AuthenticationScheme);
-        }
+		public AuthenticationController(AppDbContext context)
+		{
+			this.context = context;
+		}
 
-        [HttpGet("~/processlogin")]
-        public async Task<IActionResult> Test() => View("ProcessLogin");
-    }
+		#endregion
+
+		#region Properties
+
+
+
+		#endregion
+
+		#region Methods
+
+		[HttpGet("~/signin")]
+		public async Task<IActionResult> SignIn() => View("SignIn", await HttpContext.GetExternalProvidersAsync());
+
+		[HttpPost("~/signin")]
+		public async Task<IActionResult> SignIn([FromForm] string provider)
+		{
+			// Note: the "provider" parameter corresponds to the external
+			// authentication provider choosen by the user agent.
+			if (string.IsNullOrWhiteSpace(provider))
+			{
+				return BadRequest();
+			}
+
+			if (!await HttpContext.IsProviderSupportedAsync(provider))
+			{
+				return BadRequest();
+			}
+
+			// Instruct the middleware corresponding to the requested external identity
+			// provider to redirect the user agent to its own authorization endpoint.
+			// Note: the authenticationScheme parameter must match the value configured in Startup.cs
+			return Challenge(new AuthenticationProperties { RedirectUri = "/" }, provider);
+		}
+
+		[HttpGet("~/signout")]
+		[HttpPost("~/signout")]
+		public IActionResult SignOut()
+		{
+			// Instruct the cookies middleware to delete the local cookie created
+			// when the user agent is redirected from the external identity provider
+			// after a successful authentication flow (e.g Google or Facebook).
+			return SignOut(new AuthenticationProperties { RedirectUri = "/" },
+				CookieAuthenticationDefaults.AuthenticationScheme);
+		}
+
+		#endregion
+	}
 }
--- ArenaWeb/Controllers/Home/HomeController.cs
+++ ArenaWeb/Controllers/Home/HomeController.cs
@@ -1,14 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+﻿using ArenaWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ArenaWeb.Controllers.Home
 {
-    public class HomeController : Controller
-    {
-        [HttpGet("~/")]
-        public ActionResult Index() => View();
-    }
+	public class HomeController : Controller
+	{
+		#region Fields
+
+		private readonly AppDbContext db;
+
+		#endregion
+
+		#region Constructor
+
+		public HomeController(AppDbContext db)
+		{
+			this.db = db;
+		}
+
+		#endregion
+
+		#region Properties
+
+
+
+		#endregion
+
+		#region Methods
+
+		#region Web-API
+
+		[HttpGet("~/")]
+		public async Task<IActionResult> Index()
+		{
+			var userId = User.Claims.Where(c => c.Type.Contains("nameidentifier")).FirstOrDefault()?.Value;
+
+			if (null == userId)
+			{
+				return View();
+			}
+
+			var dbUser = await db.Twitchuser.Where(u => u.Twitchuser_Id == userId).FirstOrDefaultAsync();
+
+			if (null == dbUser || true)
+			{
+				return View("~/Views/Register/Index.cshtml");
+			}
+			else
+			{
+				return View();
+			}
+		}
+	}
+
+	#endregion
+
+	#endregion
 }
